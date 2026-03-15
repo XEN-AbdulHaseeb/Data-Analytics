a0702f780ab0f8fac3c2919e6fb9583041909f75
XEN-AbdulHaseeb <102440117+XEN-AbdulHaseeb@users.noreply.github.com>
2026-03-15 08:27:41 +0400

    Add files via upload

---
+++ Bike Rental/SQL Scripts/SQLQuerybike.sql
@@ -0,0 +1,20 @@
+WITH combined_year as( SELECT * FROM dbo.bike_share_yr_0
+UNION ALL
+SELECT * FROM dbo.bike_share_yr_1)
+
+--optimizing and joining our table with the cost table
+SELECT 
+dteday,
+season,
+mnth,
+hr,
+weekday,
+rider_type,
+riders,
+price,
+COGS,
+riders*price as revenue,
+(riders*price) - (COGS*riders) as profit
+FROM combined_year as a
+LEFT JOIN dbo.cost_table as b
+ON a.yr = b.yr
